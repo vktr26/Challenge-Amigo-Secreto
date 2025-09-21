514e1394a966cf967b34af0a5a2798e5ea429c82
Victor Aguilar <vaguilar.93@hotmail.com>
2025-09-21 18:55:44 -0300

    inicializando repositorio

---
+++ app.js
@@ -0,0 +1,49 @@
+let amigos = [];
+function agregarAmigo() {
+  let inputNombre = document.getElementById("amigo");
+  if (inputNombre.value.trim() !== "") {
+    amigos.push(inputNombre.value);
+    inputNombre.value = "";
+    mostrarAmigos();
+  } else {
+    alert("Por favor, ingresa un nombre");
+  }
+}
+
+function mostrarAmigos() {
+  let amigosList = document.getElementById("listaAmigos");
+  amigosList.innerHTML = "";
+  for (let i = 0; i < amigos.length; i++) {
+    let li = document.createElement("li");
+    li.textContent = amigos[i];
+    amigosList.appendChild(li);
+  }
+}
+
+function sortearAmigo() {
+  if (amigos.length < 2) {
+    alert("Debes ingresar al menos dos amigos para sortear.");
+    return;
+  }
+  if (amigos.length > 10) {
+    alert("No se pueden sortear más de 10 amigos.");
+    return;
+  }
+  const indice = Math.floor(Math.random() * amigos.length);
+  const amigoSorteado = amigos[indice];
+  const resultadoLista = document.getElementById("resultado");
+  const li = document.createElement("li");
+  li.textContent = "Tu amigo secreto es " + amigoSorteado;
+  resultadoLista.appendChild(li);
+}
+
+const inputAmigo = document.getElementById('amigo');
+
+inputAmigo.addEventListener('keydown', function(e) {
+    if (e.key === 'Enter') {
+        e.preventDefault();
+        agregarAmigo();
+        inputAmigo.value = '';
+        inputAmigo.focus();
+    }
+});
